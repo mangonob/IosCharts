9b90a6e578a7225a83351fe55936949e3fddef7e
mangonob <534393806@qq.com>
2016-08-19 00:42:31 +0800

    fix the Header Search issue

---
+++ IosCharts/IosCharts-Bridge.h
@@ -0,0 +1,15 @@
+//
+//  IosCharts-Bridge.h
+//  IosCharts
+//
+//  Created by Trinity on 16/8/19.
+//  Copyright © 2016年 Trinity. All rights reserved.
+//
+
+#ifndef IosCharts_Bridge_h
+#define IosCharts_Bridge_h
+
+#import "ELineChart.h"
+#import "ZCharsManager.h"
+
+#endif /* IosCharts_Bridge_h */
--- IosCharts/ViewController.swift
+++ IosCharts/ViewController.swift
@@ -7,27 +7,11 @@
 //
 
 import UIKit
-import Charts
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var barChart: BarChartView!
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let unitsSold = [1.2, 2.3, 3.4, 4.5, 5.6, 2.3]
-        let months : [NSObject] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
-        var idx = 0
-        let dataEntries = unitsSold.map { (value) -> BarChartDataEntry in
-            let entry = BarChartDataEntry(value: value, xIndex: idx)
-            idx = idx + 1
-            return entry
-        }
-        
-        let dataSet = BarChartDataSet(yVals: dataEntries, label: "aklsjkfld")
-        
-        print(dataSet is IChartDataSet)
     }
 }
 
